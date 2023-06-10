1813746a7951d58c0f17bac12ccbb1ac880024b0
Mutie ur Rehman <mutierehman12@gmail.com>
2023-06-10 05:59:08 -0700

    removed comment

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\ContactController; 
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
@@ -30,13 +26,27 @@ Route::get('/', function () {
     Route::get("/myItem", [MainController::class, 'item']);
 
 
+
+
    /* |--------------------------------------------------------------------------
    |  Contact Routes 
    |-------------------------------------------------------------------------- */ 
    Route::get('/contact-us', [ContactController::class, 'index']);
    Route::post('/contact-us', [ContactController::class, 'store'])->name('contact.us.store');
 
+  
 
+    /* |--------------------------------------------------------------------------
+   |  Post and Review Routes 
+   |-------------------------------------------------------------------------- */ 
 
+// Manage Post
+Route::get("/myReview", [PostController::class, 'create'])->name('post.create');
+Route::post('/post-store',[PostController::class, 'store'])->name('post.store');
+Route::get('/post-list',[PostController::class, 'list'])->name('post.list');
+Route::get('/post-view/{id}',[PostController::class, 'view'])->name('post.view');
+ 
+// Manage Review
+Route::post('/review-store',[PostController::class, 'reviewstore'])->name('review.store');
 
 
